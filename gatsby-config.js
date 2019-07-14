0d683b247c47a1d0093cf3bc62c3af30176e1c87
Rémy Cousin <rem.cousin@gmail.com>
2019-07-14 17:34:31 +0200

    Updated APiurl config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,9 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: process.env.DEPLOY_URL
+          ? "https://gatsby-strapi-postgresql-demo.herokuapp.com"
+          : "http://localhost:1337",
         contentTypes: [
           'article',
           'user'
